95af91ddeb4483cbbfa56e972ee4651e5c8ef631
Naomi Ahmed <yretenai@gmail.com>
2021-12-30 18:25:24 +0000

    rollback d4239e1e (i'm dum)

--- Snuggle/Handlers/SnuggleCore.cs
+++ Snuggle/Handlers/SnuggleCore.cs
@@ -128,6 +128,10 @@ public class SnuggleCore : Singleton<SnuggleCore>, INotifyPropertyChanged, IDisp
                 } catch (Exception e) {
                     LogTarget.Error("Worker", $"Failed to perform {name} task", e);
                 }
+                
+                LogTarget.Info("Worker", $"Memory Tension: {GC.GetTotalMemory(false).GetHumanReadableBytes()}");
+                IsFree = true;
+                Dispatcher.Invoke(() => OnPropertyChanged(nameof(IsFree)));
             }
         } catch (TaskCanceledException) {
             // ignored
@@ -135,10 +139,6 @@ public class SnuggleCore : Singleton<SnuggleCore>, INotifyPropertyChanged, IDisp
             // ignored
         } catch (Exception e) {
             LogTarget.Error("Worker", "Failed to get tasks", e);
-        } finally {
-            LogTarget.Info("Worker", $"Memory Tension: {GC.GetTotalMemory(false).GetHumanReadableBytes()}");
-            IsFree = true;
-            Dispatcher.Invoke(() => OnPropertyChanged(nameof(IsFree)));
         }
     }
 
